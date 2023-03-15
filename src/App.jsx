5549700a7352385928bccb4acd47ab1a9052a556
Lucas <48570788+zaluk365@users.noreply.github.com>
2023-03-14 23:49:37 -0300

    feat: add new features Add deleteElement function Refactor addElement function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
 import Button from "./components/Button"
 
+const elementsURL = "http://localhost:3000/elements"
+
 function App() {
+  
+  const [elements, setElements] = useState([])
 
   const addElement = () => {
     const payload = {
-      manu: "Chiquito y contento"
+      name: "Manu",
+    description: "Pituso",
+    atomicNumber: 1,
+    mass: 1,
+    abbreviation: "Man"
     }
-    const url = 'http://localhost:3000/elements/new'
-    axios.post(url, payload).then((res) => {
-      console.log(res.data)
+    axios.post(`${elementsURL}/new`, payload).then((res) => {
+      getElements()
     })
   }
 
+  const getElements = () => {
+    axios.get(elementsURL).then((res) => {
+      setElements(res.data)
+    })
+  }
+
+  const deleteElement = (id) => {
+    console.log('deleteElemnt')
+    axios.delete(`${elementsURL}/delete/${id}`).then((res) => {
+      getElements()
+    })
+  }
+
+  useEffect(() => {
+    getElements()
+  }, [])
+  useEffect(() => {
+    console.log('hola', elements)
+  }, [elements])
   return (
     <div className="App">
       <h1>soy yo</h1>
+      <ul>{elements.map((element) => {
+        return (
+          <li>
+            <p>{element.name}</p>
+            <button onClick={() => deleteElement(element._id)}>borrar</button>
+            </li>
+        )
+      })}</ul>
       <Button handleAction={addElement}/>
     </div>
   )
