ba6715bcb702637f71a6edb18228b3aadfdbc35d
Lucas <48570788+zaluk365@users.noreply.github.com>
2023-03-20 23:12:49 -0300

    refactor: change app logic and organization

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,40 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Button from "./components/Button";
 import Element from "./components/Element";
+import Modal from "./components/Modal"
+import {addElement, getElements, deleteElement} from "./utils/handleElement"
 import "./assets/app.css";
-
-const elementsURL = "http://localhost:3000/elements";
 
 function App() {
   const [elements, setElements] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const addElement = () => {
-    const payload = {
-      name: "Manu",
-      description: "Pituso",
-      atomicNumber: 1,
-      mass: 1,
-      abbreviation: "Man",
-    };
-    axios.post(`${elementsURL}/new`, payload).then((res) => {
-      getElements();
-    });
-  };
 
-  const getElements = () => {
-    axios.get(elementsURL).then((res) => {
-      setElements(res.data);
-    });
-  };
-
-  const deleteElement = (id) => {
-    console.log("deleteElemnt");
-    axios.delete(`${elementsURL}/delete/${id}`).then((res) => {
-      getElements();
-    });
-  };
+  const getElementsData = async () => {
+    const response = await getElements();
+    console.log(response)
+    setElements(response.data);
+  }
 
   useEffect(() => {
-    getElements();
+    getElementsData()
   }, []);
-  useEffect(() => {
-    console.log("hola", elements);
-  }, [elements]);
   return (
     <div className="app">
-      {showModal && <div className="modal" onClick={() => {setShowModal(!showModal)}}></div>}
+      {showModal && (
+        <Modal showModal={showModal} setShowModal={setShowModal}/>
+      )}
       <h1>soy yo</h1>
       <div className="container">
-      <ul className="list">
-        {elements.map((element) => {
-          return <Element element={element} deleteElement={deleteElement} />;
-        })}
-      </ul>
-      <Button handleAction={addElement} showModal={showModal} setShowModal={setShowModal}/>
+        <ul className="list">
+          {elements.map((element) => {
+            return <Element element={element} key={element.id} />;
+          })}
+        </ul>
+        <Button
+          handleAction={addElement}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
       </div>
     </div>
   );
